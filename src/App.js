305de192ff90b150289c9a80e9d88af4dcd0db13
saikab <spsaikab@gmail.com>
2023-02-16 14:20:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+
+import { Board } from "./components/Board";
+import { ResetButton } from "./components/ResetButton";
+import { ScoreBoard } from "./components/ScoreBoard";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [1, 2, 3],
+    [4, 5, 6],
+    [5, 6, 7],
+    [8, 9, 10],
+    [9, 10, 11],
+    [12, 13, 14],
+    [13, 14, 15],
+    [0, 4, 8],
+    [4, 8 , 12],
+    [1, 5, 9],
+    [5, 9, 13],
+    [2, 6, 10],
+    [6, 10 , 14],
+    [3, 7, 11],
+    [7, 11, 15],
+    [0, 5, 10],
+    [5, 10, 15],
+    [1, 6, 11],
+    [4, 9, 14],
+    [2, 5, 8],
+    [3, 6, 9],
+    [12, 9, 16],
+    [7, 10, 13]
+    
+  ]
+
+  const [xPlaying, setXPlaying] = useState(true);
+  const [board, setBoard] = useState(Array(16).fill(null))
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 })
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleBoxClick = (boxIdx) => {
+    // Step 1: Update the board
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying ? "X" : "O";
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard);
+
+    // Step 2: Check if either player has won the game
+    const winner = checkWinner(updatedBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1;
+        setScores({ ...scores, oScore })
+      } else {
+        let { xScore } = scores;
+        xScore += 1;
+        setScores({ ...scores, xScore })
+      }
+    }
+
+    // Step 3: Change active player
+    setXPlaying(!xPlaying);
+  }
+
+  const checkWinner = (board) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
+
+      // Iterate through win conditions and check if either player satisfies them
+      if (board[x] && board[x] === board[y] && board[y] === board[z] && board[z] ) {
+        setGameOver(true);
+        return board[x];
+      }
+    }
+  }
+
+  const resetBoard = () => {
+    setGameOver(false);
+    setBoard(Array(16).fill(null));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick} />
+      <ResetButton resetBoard={resetBoard} />
     </div>
   );
 }
